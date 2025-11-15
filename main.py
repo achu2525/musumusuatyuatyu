08b7067e235b0177917ad6b5b81c75000d1ce665
achu2525 <achunnnn2525@gmail.com>
2025-11-15 13:20:50 +0900

    Update main.py

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from discord.ext import commands
 import time
 import re
+import os
 
 intents = discord.Intents.default()
 intents.message_content = True
@@ -49,6 +50,3 @@ async def on_message(message):
 
 # Bot起動
 bot.run(os.getenv("BOT_TOKEN"))
-
-
-
